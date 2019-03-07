1f07c6fcaa368f222ed89457e407bbe1481bf617
Ricardo Morales <ricky@Rickys-Computer.home>
2019-03-07 13:29:31 -0500

    Add Shift+D custom duplicate command

--- __Core/Editor/EditorDuplicateCommand.cs
+++ __Core/Editor/EditorDuplicateCommand.cs
@@ -10,11 +10,22 @@ public static class EditorDuplicateCommand
     {
         List<GameObject> newSelection = new List<GameObject>();
         for (int ui = 0; ui < Selection.gameObjects.Length; ui++) {
+        
             GameObject go = Selection.gameObjects[ui];
             int siblingIndex = go.transform.GetSiblingIndex();
-            GameObject newGo = GameObject.Instantiate(go);
+
+            GameObject newGo;
+
+            GameObject prefabRoot = PrefabUtility.GetCorrespondingObjectFromSource(go) as GameObject;
+            if (prefabRoot != null) {
+                newGo = (GameObject)PrefabUtility.InstantiatePrefab(prefabRoot);
+                PrefabUtility.SetPropertyModifications(newGo, PrefabUtility.GetPropertyModifications(go));
+            } else {
+                newGo = GameObject.Instantiate(go);
+            }
             newGo.transform.parent = go.transform.parent;
             newGo.transform.position = go.transform.position;
+            newGo.transform.localScale = go.transform.localScale;
             newGo.transform.SetSiblingIndex(siblingIndex + 1);
             Undo.RegisterCreatedObjectUndo(newGo, "Duplicate");
             newGo.name = go.name;
